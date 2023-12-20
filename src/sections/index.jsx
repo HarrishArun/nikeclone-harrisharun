ce6ba58cafff288c3e6c63817f9637a3e8cbd4ca
HarrishArun <95520465+HarrishArun@users.noreply.github.com>
2023-12-20 20:21:58 +0530

    finalcommit

---
+++ src/sections/index.jsx
@@ -0,0 +1,19 @@
+import Hero from "./hero";
+import PopularProducts from "./popularproducts";
+import SuperQuality from "./superquality";
+import Subscribe from "./subscribe";
+import Services from "./services";
+import SpecialOffers from "./specialoffers";
+import Footer from "./footer";
+import CustomerReviews from "./CustomerReviews";
+export{
+    Hero,
+    PopularProducts,
+
+    Services,
+    SpecialOffers,
+    CustomerReviews,
+    Footer,
+    Subscribe,
+    SuperQuality
+}
